b633b85f6ea19f807d5ff7cfd0fa3340000e98f1
Eva Bespalova <evysia6@gmail.com>
2023-11-29 22:47:08 +0500

    решение первой задачи

--- zadanie001/Program.cs
+++ zadanie001/Program.cs
@@ -3,5 +3,19 @@
 //массива, значения которых лежат в отрезке [20,90].
 // массив [10 21 14 93 23] => 2
 
-
-
+int [] myArray = new int [10];
+int result = 0;
+for(int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = new Random().Next(0,101);
+}
+foreach(int element in myArray)
+{
+    Console.Write(element + " ");
+    if (element >= 20 && element <= 90)
+    {
+        result++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Количесивтво элементов, соотвествующих условию: " + result);
