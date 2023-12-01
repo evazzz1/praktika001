d14fd1865927a97dcc5d29db6316a3454da91713
Eva Bespalova <evysia6@gmail.com>
2023-12-02 02:22:54 +0500

    Решила четвертую задачу

--- zadanie004/Program.cs
+++ zadanie004/Program.cs
@@ -6,4 +6,28 @@
 //8741 => [8 7 4 1]
 //4 => [4]
 
-Console.WriteLine("Hello, World!");
+int number = new Random().Next(1,100001);
+int num = number;
+Console.WriteLine(num);
+int countOfNumbers = 0;
+for(int i = 0; i < 6; i++)
+{
+    if(num > 0)
+    {
+        countOfNumbers++;
+    }
+    num /= 10;
+}
+Console.WriteLine(countOfNumbers);
+int [] myArray = new int [countOfNumbers];
+
+for(int i = myArray.Length-1; i >= 0; i--)
+{
+    myArray[i] = number%10;
+    number /= 10; 
+}
+
+foreach (int element in myArray)
+{
+    Console.Write(element + " ");
+}
