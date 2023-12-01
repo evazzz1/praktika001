8c88a8a5aa13c5d081580f1b873766915bb6ee86
Eva Bespalova <evysia6@gmail.com>
2023-12-02 01:51:44 +0500

    Решила вторую и третью задачу

--- zadanie002/Program.cs
+++ zadanie002/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте массив на 10 целых чисел. Напишите программу, 
+//которая определяет количество чётных чисел в массиве.
+//массив [6 7 19 34 3 1 4 7 9 1] => 3 
+
+int [] myArray = new int [10];
+int result = 0;
+for(int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = new Random().Next(0,101);
+}
+foreach(int element in myArray)
+{
+    Console.Write(element + " ");
+    if (element % 2 ==0)
+    {
+        result++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Количесивтво элементов, соотвествующих условию: " + result);
--- zadanie003/Program.cs
+++ zadanie003/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+//массив [1.22 4.5 3.33] => 3.28
+
+double [] myArray = new double [10];
+int a; int b;        
+
+for(int i = 0; i < myArray.Length; i++)
+{
+    a = new Random().Next(0,101); //целая часть
+    b = new Random().Next(1,100); //число после запятой
+    myArray[i] = b * 0.01 + a;
+}
+
+double min = myArray[0]; 
+double max = myArray[0];
+
+for(int i = 0; i < myArray.Length; i++)
+{
+    Console.Write(myArray[i] + "  ");
+    if(min > myArray[i])
+    {
+        min = myArray[i];
+    }
+    if(max < myArray[i])
+    {
+        max = myArray[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine("MIN " + min + " MAX " + max + " Разница: " + (max-min));
